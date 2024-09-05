3ce8e839ba52c64d809cb70906510de91c40d0cc
Utkarsh Kumar Patel <utkarshkumar7545@gmail.com>
2024-09-05 16:16:58 +0530

    fixing bug

--- solution.js
+++ solution.js
@@ -1,20 +1,36 @@
 //  1  Given the dataset of individuals, write a function that accesses and returns the email addresses of all individuals.
 
-const getEmails = (arrayOfObjects) => {
-    const allEmails = [];
-    for(let i=0; i<arrayOfObjects.length; i++){
-        allEmails[i] = arrayOfObjects[i].email;   //  allEmails[i].push(arrayOfObjects[i].email);
+const getEmails = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
     }
-    return allEmails;
+    else{
+        const allEmails = [];
+        for(let i=0; i<data.length; i++){
+            if(data[0].email){
+                allEmails[i] = data[i].email;   //  allEmails[i].push(data[i].email);
+            }
+            else{
+                return "Email not found.";
+            }
+        }
+        return allEmails;
+    }
 }
 
 
 //  2  Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
 
-const getHobbies = (arrayOfObjects) => {
-    for(let i=0; i<arrayOfObjects.length; i++){
-        if(30 === arrayOfObjects[i].age){
-            console.log(`Hobbies: ${arrayOfObjects[i].hobbies}`);
+const getHobbies = (data, ag) => {
+    
+    if(!data || !Array.isArray(data) || data.length === 0 || typeof ag !== 'number'){
+        return "Invalid Data.";
+    }
+    else{
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].age === ag){
+                console.log(`Hobbies: ${data[i].hobbies}`);
+            }
         }
     }
 }
@@ -22,10 +38,15 @@ const getHobbies = (arrayOfObjects) => {
 
 //   3  Create a function that extracts and displays the names of individuals who are students (isStudent: true) and live in Australia.
 
-const getNames = (arrayOfObjects) => {
-    for(let i=0; i<arrayOfObjects.length; i++){
-        if((arrayOfObjects[i].isStudent) && arrayOfObjects[i].country === "Australia"){
-            console.log(`Names: ${arrayOfObjects[i].name}`);
+const getNames = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
+    }
+    else{
+        for(let i=0; i<data.length; i++){
+            if((data[i].isStudent) && data[i].country === "Australia"){
+                console.log(`Names: ${data[i].name}`);
+            }
         }
     }
 }
@@ -33,46 +54,70 @@ const getNames = (arrayOfObjects) => {
 
 //   4  Write a function that accesses and logs the name and city of the individual at the index position 3 in the dataset.
 
-const getName_City = (arrayOfObjects) => {
-    console.log(`Name: ${arrayOfObjects[3].name}, City: ${arrayOfObjects[3].city}`);
+const getName_City = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
+    }
+    else{
+        console.log(`Name: ${data[3].name}, City: ${data[3].city}`);
+    }
 }
 
 
 //   5  Implement a loop to access and print the ages of all individuals in the dataset.
 
-const getAges = (arrayOfObjects) => {
-    for(let i=0; i<arrayOfObjects.length; i++){
-        console.log(`Ages: ${arrayOfObjects[i].age}`);
+const getAges = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
+    }
+    else{
+        for(let i=0; i<data.length; i++){
+            console.log(`Ages: ${data[i].age}`);
+        }
     }
 }
 
 
 //   6  Create a function to retrieve and display the first hobby of each individual in the dataset.
 
-const getFirstHobby = (arrayOfObjects) => {
-    for (let i = 0; i < arrayOfObjects.length; i++) {
-        console.log(`First Hobby: ${arrayOfObjects[i].hobbies[0]}`);
+const getFirstHobby = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
     }
+    else{
+        for (let i = 0; i < data.length; i++) {
+            console.log(`First Hobby: ${data[i].hobbies[0]}`);
+        }
+    }  
 }
 
 
 //   7  Write a function that accesses and prints the names and email addresses of individuals aged 25.
 
-const getNames_Emails = (arrayOfObjects) => {
-    for (let i = 0; i < arrayOfObjects.length; i++) {
-        if(arrayOfObjects[i].age === 25){
-            console.log(`Name: ${arrayOfObjects[i].name}, Email: ${arrayOfObjects[i].email}`);
+const getNames_Emails = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
+    }
+    else{
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].age === 25){
+                console.log(`Name: ${data[i].name}, Email: ${data[i].email}`);
+            }
         }
-        
     }
 }
 
 
 //   8  Implement a loop to access and log the city and country of each individual in the dataset.
 
-const getCity_Country = (arrayOfObjects) => {
-    for (let i = 0; i < arrayOfObjects.length; i++) {
-        console.log(`City: ${arrayOfObjects[i].city}, Country: ${arrayOfObjects[i].country}`);
+const getCity_Country = (data) => {
+    if(!data || !Array.isArray(data) || data.length === 0){
+        return "Invalid Data.";
+    }
+    else{
+        for (let i = 0; i < data.length; i++) {
+            console.log(`City: ${data[i].city}, Country: ${data[i].country}`);
+        }
     }
 }
 
--- test.js
+++ test.js
@@ -1,6 +1,85 @@
 import { arrayOfObjects } from "./persons.js";
 import { getEmails, getHobbies, getNames, getAges, getCity_Country, getFirstHobby, getName_City, getNames_Emails } from "./solution.js";
 
-const len = arrayOfObjects.length;
+console.log("1:");
 
+const func1 = () => {
+    console.log(getEmails(arrayOfObjects));
+    console.log(getEmails([]));
+    console.log(getEmails(26));
+    console.log(getEmails("utkarsh"));
+}
+
+console.log("2:");
+
+const func2 = () => {
+    console.log(getHobbies(arrayOfObjects, 30));
+    console.log(getHobbies(arrayOfObjects, 0));   
+    console.log(getHobbies([], 45));        
+    console.log(getHobbies('c', 10));
+}
+
+console.log("3:");
+
+const func3 = () => {
+    console.log(getNames(arrayOfObjects));
+    console.log(getNames("&$_"));
+    console.log(getNames(54.12));
+    console.log(getNames({}));
+}
+
+console.log("4:");
+
+const func4 = () => {
+    console.log(getName_City(arrayOfObjects));
+    console.log(getName_City([]));
+    console.log(getName_City(0));
+    console.log(getName_City("amit"));
+}
+
+console.log("5:");
+
+const func5 = () => {
+    console.log(getAges(arrayOfObjects));
+    console.log(getAges("011562"));
+    console.log(getAges(101));
+    console.log(getAges([]));
+}
+
+console.log("6:");
+
+const func6 = () => {
+    console.log(getFirstHobby(arrayOfObjects));
+    console.log(getFirstHobby("hello"));
+    console.log(getFirstHobby(2679.12));
+    console.log(getFirstHobby([]));
+}
+
+console.log("7:");
+
+const func7 = () => {
+    console.log(getNames_Emails(arrayOfObjects));
+    console.log(getNames_Emails(0.2456));
+    console.log(getNames_Emails([]));
+    console.log(getNames_Emails("thank you"));
+}
+
+console.log("8:");
+
+const func8 = () => {
+    console.log(getCity_Country(arrayOfObjects));
+    console.log(getCity_Country('hii'));
+    console.log(getCity_Country(24.236));
+    console.log(getCity_Country([]));
+}
+
+
+func1();
+func2();
+func3();
+func4();
+func5();
+func6();
+func7();
+func8();
 
