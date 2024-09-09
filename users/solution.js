e67c58529302a213fdd8713df4ec9aac3802cd80
Utkarsh Kumar Patel <utkarshkumar7545@gmail.com>
2024-09-09 10:55:03 +0530

    func1 and func2

--- users/solution.js
+++ users/solution.js
@@ -1,14 +1,37 @@
 // Q1 Find all users who are interested in playing video games.
 
-const getAllUsers = (data) => {
+const checkInterest = (data) => {
     const arr = [];
-    for(let user in data){
-        if((data[user].interests) && data[user].interests[0].includes("Playing Video Games")){
-            arr.push(user);
-        }
+    if(!data || typeof data !== 'object' || Object.keys(data).length === 0){
+        return "Invalid Data";
     }
-    return arr;
+    else{
+        for(let user in data){
+            if((data[user].interests) && data[user].interests[0].includes("Video Games")){
+                arr.push(user);
+            }
+        }
+        return arr;
+    }
 }
 
-export { getAllUsers };
+// Q2 Find all users staying in Germany.
+
+const checkNationality = (data) => {
+    if(!data || typeof data !== 'object' || Object.keys(data).length === 0){
+        return "Invalid Data";
+    }
+    else{
+        const arr = [];
+        for(let user in data){
+            if((data[user].nationality) && data[user].nationality.includes("Germany")){
+                arr.push(user);
+            }
+        }
+        return arr;
+    }
+}
+
+
+export { checkInterest, checkNationality};
 
--- users/test.js
+++ users/test.js
@@ -1,10 +1,19 @@
 import { users } from './users.js';
-import { getAllUsers } from './solution.js';
+import { checkInterest, checkNationality } from './solution.js';
 
 const func1 = () => {
-    console.log(getAllUsers(users));
-    
+    console.log(checkInterest(users));
+    console.log(checkInterest([]));
+    console.log(checkInterest({}));
+    console.log(checkInterest("users"));
+}
+
+const func2 = () => {
+    console.log(checkNationality(users));
+    console.log(checkNationality(24));
+    console.log(checkNationality('24'));
+    console.log(checkNationality({}));
 }
 
 func1();
-
+func2();
