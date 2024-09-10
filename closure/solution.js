8b4fe0acb9b6863a9f6376716bebfd5aaf5c1c85
Utkarsh Kumar Patel <utkarshkumar7545@gmail.com>
2024-09-11 00:18:14 +0530

    limitFunctionCallCount

--- closure/solution.js
+++ closure/solution.js
@@ -1,8 +1,19 @@
- import { counterFactory, limitFunctionCallCount, cacheFunction } from "./closure.js";
+import { counterFactory, limitFunctionCallCount, cacheFunction } from "./closure.js";
 
+// 1:
 
 console.log(counterFactory().increment());
 console.log(counterFactory().decrement());
+
+// 2:
+
+const x = limitFunctionCallCount((data) => data * data, 6);
+
+for(let i=0; i<9; i++){
+    console.log(x(10));
+}
+
+// 3:
 
 
 
